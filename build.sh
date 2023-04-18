ea0ed9c8fe9b8a64a9cfcd7debd9bf346265dff4
Daniel Kříž <Daniel.kriz@protonmail.com>
2023-04-18 23:24:18 +0200

    build_script: do not delete whole cache

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-rm -rf build/
+# rm -rf build/
 # rm -rf diffkemp/simpll/_simpll.abi3.so
-cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release
+rm -rf build/diffkemp/simpll/libsimpll-lib.a
+cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release -DSIMPLL_REBUILD_BINDINGS=1
 cmake --build build
 pip install -e .
