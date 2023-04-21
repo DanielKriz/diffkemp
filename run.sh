54e4f78a1c0113ac5087a305b25f350310e1176b
Daniel Kříž <Daniel.kriz@protonmail.com>
2023-04-21 10:40:42 +0200

    run: add conf option

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ if [ "$1" = "debug" ]; then
     gdb --ex=r --args ./build/diffkemp/simpll/diffkemp-simpll pattern --fun=add Runtime/test1/main.ll
 elif [ "$1" = "test" ]; then
     ./tests/unit_tests/simpll/runTests
+elif [ "$1" = "conf" ]; then
+    ./bin/diffkemp generate -c Runtime/pattern_config.yaml
 else
     ./bin/diffkemp generate -f add Runtime/test1/main.ll Runtime/test2/main.ll
 fi
