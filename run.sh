f2b54bd1d7d765544a07cadf8acc2d3c7eef6cbb
Daniel Kříž <Daniel.kriz@protonmail.com>
2023-04-21 10:40:31 +0200

    run: execute gbd

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "debug" ]; then
-    gdb --args ./build/diffkemp/simpll/diffkemp-simpll pattern --fun=add Runtime/test1/main.ll Runtime/test2/main.ll
+    gdb --ex=r --args ./build/diffkemp/simpll/diffkemp-simpll pattern --fun=add Runtime/test1/main.ll
 elif [ "$1" = "test" ]; then
     ./tests/unit_tests/simpll/runTests
 else
