14306bb07536bc5323d5a4ec9db0ad0c9feeea2b
Daniel Kříž <Daniel.kriz@protonmail.com>
2023-04-21 10:38:39 +0200

    build: prefer debug build instead of release build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # rm -rf build/
 # rm -rf diffkemp/simpll/_simpll.abi3.so
 rm -rf build/diffkemp/simpll/libsimpll-lib.a
-cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release -DSIMPLL_REBUILD_BINDINGS=1
+cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Debug -DSIMPLL_REBUILD_BINDINGS=1
 cmake --build build
 pip install -e .
