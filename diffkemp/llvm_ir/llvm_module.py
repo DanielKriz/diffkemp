e5e78f1b6a3a8ea9e141ef37b9eb321c7949261d
Daniel Kříž <Daniel.kriz@protonmail.com>
2024-06-06 11:12:05 +0200

    simpll_lib: remove the need to check build directory
    There are two cases where this script is used.
    
    First, in the development where the first part is called and we look for
    the `_simpll` inside our development directory. In the previous
    implementation we relied on the `SIMPLL_BUILD_DIR` which has one
    downsight where some possible attacker could change this variable and
    then our library would link towards different library.
    
    Second, in the release environment we assume that the `diffkemp` library
    is installed somewhere where it could be seen by the python interpreter,
    presumably site-packages.
    
    What is important for us is, that during development we are already
    setting the PYTHONPATH to our build directory in our generated
    development executable.

--- diffkemp/llvm_ir/llvm_module.py
+++ diffkemp/llvm_ir/llvm_module.py
@@ -4,7 +4,7 @@ Functions for working with modules and parameters in them.
 """
 
 from diffkemp.simpll.library import SimpLLModule
-from diffkemp.simpll._simpll.lib import shutdownSimpLL
+from diffkemp.simpll.simpll_lib import lib
 from diffkemp.utils import get_opt_command
 import os
 import re
