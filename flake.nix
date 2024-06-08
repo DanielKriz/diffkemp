e9679a721f2e464cc2d81c110ff43362926d37b9
Daniel Kříž <Daniel.kriz@protonmail.com>
2024-06-09 00:12:24 +0200

    Allow LLVM 18 in nix
    For that we have to move to newer version of nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Static analyser of semantic differences in large C projects";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/release-23.11"; };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/release-24.05"; };
 
   outputs = { self, nixpkgs, ... }:
     let
@@ -9,7 +9,7 @@
       pkgs = import nixpkgs { inherit system; };
 
       llvmVersionMin = 9;
-      llvmVersionMax = 17;
+      llvmVersionMax = 18;
       llvmVersions = pkgs.lib.lists.range llvmVersionMin llvmVersionMax;
 
       mkDiffkemp =
